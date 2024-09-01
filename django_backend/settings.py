2357f7badfaf9edc049822d287318c3184973951
Halal Kharbouch <binyaminbgod@gmail.com>
2024-09-01 16:42:57 +0100

    commit

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'ygfsdtygfuewgytegwywebyvvy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
