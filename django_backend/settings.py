9a8bc72916a101a763c09de3a5f90d38a898e68b
Halal Kharbouch <binyaminbgod@gmail.com>
2024-09-04 22:05:56 +0100

    update origin

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -153,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cross Origin Settings
 CORS_ALLOWED_ORIGINS = [
-    "https://ideasassesss.onrender.com",  # React app URL
+    "https://ideas-assess.onrender.com/",  # React app URL
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
